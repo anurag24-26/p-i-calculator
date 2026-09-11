1bc84d29606009cf18a5c3654ec626243602be18
Anurag Tripathi <113921508+anurag24-26@users.noreply.github.com>
2026-09-11 08:05:55 +0530

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -1,11 +1,11 @@
-import type { MetadataRoute } from "next";
+import type { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
-      userAgent: "*",
-      allow: "/",
+      userAgent: '*',
+      allow: '/',
     },
-    sitemap: "https://protein-calculator.example.com/sitemap.xml",
-  };
+    sitemap: 'https://yourproteinintake.vercel.app/sitemap.xml',
+  }
 }
